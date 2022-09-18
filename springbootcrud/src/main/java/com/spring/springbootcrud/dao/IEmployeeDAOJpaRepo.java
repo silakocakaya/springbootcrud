e18b65e965b3853bb1acefd04e43629b735d03f8
silakocakaya <67272114+silakocakaya@users.noreply.github.com>
2022-09-18 14:38:44 +0300

    jpa repo rest

--- springbootcrud/src/main/java/com/spring/springbootcrud/dao/IEmployeeDAOJpaRepo.java
+++ springbootcrud/src/main/java/com/spring/springbootcrud/dao/IEmployeeDAOJpaRepo.java
@@ -1,9 +1,11 @@
 package com.spring.springbootcrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.spring.springbootcrud.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface IEmployeeDAOJpaRepo extends JpaRepository<Employee, Integer>{
 
 }
